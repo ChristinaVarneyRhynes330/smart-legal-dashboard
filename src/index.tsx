64b069c17f97d3e4a3bef35c08391eb428f0ce5e
ChristinaVarneyRhynes330 <chrissyvarneyrynes@gmail.com>
2025-08-03 20:22:19 -0400

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import ChildWelfareDashboard from './pages/ChildWelfareDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<ChildWelfareDashboard />);
